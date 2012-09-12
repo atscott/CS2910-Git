3241253852b894c69611a40828422191b11f405b
Andrew Scott <atscott01@gmail.com>
2012-09-12 13:50:21 -0500

    Initial Program code

---
+++ CS2910-Lab1-HTTPClient/src/client/HttpClient.java
@@ -0,0 +1,173 @@
+package client;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+import java.net.UnknownHostException;
+
+/*
+ * Course: CS2910
+ * Term: Sept. 2012
+ * Assignment: Lab 1
+ * Author: Andrew Scott
+ * Date: 9/10/12
+ */
+
+/**
+ * a simple Http client that sends a request to a web server and interprets the
+ * response.
+ */
+public class HttpClient {
+
+	/**
+	 * The address of the host
+	 */
+	private String host;
+
+	/**
+	 * The port that the client will use
+	 */
+	private int port;
+
+	/**
+	 * socket for the client
+	 */
+	private Socket socket;
+
+	/**
+	 * 
+	 */
+	private DataOutputStream transmitStream;
+
+	/**
+	 * The end-of-line string that will work for any system
+	 */
+	final static String CRLF = "\r\n";
+
+
+
+	/**
+	 * @param host
+	 * @param port
+	 */
+	HttpClient(String host, int port) {
+		this.host = host;
+		this.port = port;
+	}
+
+	/**
+	 * 
+	 */
+	public void run() {
+		boolean ok = openSocket();
+
+		if (ok) {
+			ok = initTransmit();
+		}
+
+		if (ok) {
+			ok = sendRequest();
+		}
+		
+		if (ok) {
+			System.out.println(getResponse());
+		}
+
+		if (ok) {
+			System.out.println("run: successful exit");
+		} else {
+			System.out.println("run: error exit");
+		}
+
+	}
+
+	private boolean openSocket() {
+		boolean ok = false;
+
+		try {
+			socket = new Socket(this.host, port);
+			ok = true;
+			System.out.println("openSocket: socket created");
+		} catch (UnknownHostException e) {
+			System.out.println("openSocket: UnknownHostException");
+		} catch (IOException e) {
+			System.out.println("openSocket: IOException");
+		}
+
+		return ok;
+	}
+
+	private boolean initTransmit() {
+		boolean ok = false;
+
+		try {
+			this.transmitStream = new DataOutputStream(socket.getOutputStream());
+			ok = true;
+			System.out.println("transmitStream: stream created");
+		} catch (IOException e) {
+			System.out.println("transmitStream: IOException");
+		}
+
+		return ok;
+	}
+
+	private boolean sendRequest() {
+		boolean ok = false;
+
+		String request = "GET / HTTP/1.1" + HttpClient.CRLF + "Host: " + host
+				+ HttpClient.CRLF + HttpClient.CRLF;
+
+		try {
+			transmitStream.writeBytes(request);
+			ok = true;
+			System.out.println("sendRequest: request sent");
+		} catch (IOException e) {
+			System.out.println("sendRequest: IOException");
+		}
+
+		return ok;
+	}
+
+	private String getResponse() {
+
+		StringBuilder sb = new StringBuilder();
+
+		try {
+			socket.setSoTimeout(3000);
+
+			
+			BufferedReader responseFromServer = new BufferedReader(
+					new InputStreamReader(socket.getInputStream()));
+
+//			InputStream input = socket.getInputStream();
+//			boolean cont = true;
+//			while(cont){
+//				byte[] buffer = new byte[1024];
+//				int len = input.read(buffer);
+//				if(len < 0){
+//					cont = false;
+//				}else{
+//					sb.append(new String(buffer));
+//				}
+//				
+//			}
+			
+			
+			int result;
+			while ((result = responseFromServer.read()) != -1) {
+				sb.append(Character.toChars(result));
+			}
+
+		} catch (SocketTimeoutException e) {
+			System.out.println("getResponse: SocketTimeoutException");
+		} catch (IOException e) {
+			System.out.println("getResponse: IOException");
+		}
+
+		return sb.toString();
+	}
+
+}
