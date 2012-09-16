3bb263e0998e78196abd2b2901eec603992d6542
Marius Volkhart <Marius@volkhart.com>
2012-09-16 14:28:23 -0500

    Added support for HTML parsing both chunked & unchunked

--- CS2910-Lab1-HTTPClient/src/client/HttpClient.java
+++ CS2910-Lab1-HTTPClient/src/client/HttpClient.java
@@ -1,3 +1,4 @@
+
 package client;
 
 import java.io.BufferedReader;
@@ -7,6 +8,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
 import java.net.UnknownHostException;
@@ -15,128 +17,191 @@ import client.Constants.MimeType;
 
 public class HttpClient {
 
-	public static final String CRLF = "\r\n";
+    public static final String CRLF = "\r\n";
 
-	private static BufferedReader bufferedReader;
-	private static DataOutputStream outputStream;
-	private static FileOutputStream fos;
-	private static Socket socket;
-	private static File textFile;
-	private static HTTPHeader header;
+    private static BufferedReader bufferedReader;
+    private static DataOutputStream outputStream;
+    private static FileOutputStream fos;
+    private static Socket socket;
+    private static File textFile;
+    private static HTTPHeader header;
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		UI ui = new UI();
-	}
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        UI ui = new UI();
+    }
 
-	public static void startRequest(String hostName, int port, String resource) {
-		try {
-			socket = new Socket(hostName, port);
-			bufferedReader = new BufferedReader(new InputStreamReader(
-					socket.getInputStream()));
-			outputStream = new DataOutputStream(socket.getOutputStream());
-			textFile = new File("text.txt");
-			fos = new FileOutputStream(textFile);
-			socket.setSoTimeout(1000);
-			header = new HTTPHeader();
+    public static void startRequest(String hostName, int port, String resource) {
+        try {
+            socket = new Socket(hostName, port);
+            bufferedReader = new BufferedReader(new InputStreamReader(
+                    socket.getInputStream()));
+            outputStream = new DataOutputStream(socket.getOutputStream());
+            textFile = new File("text.txt");
+            textFile.delete();
+            textFile.createNewFile();
+            fos = new FileOutputStream(textFile, true);
+            socket.setSoTimeout(1000);
+            header = new HTTPHeader();
 
-			outputStream.writeBytes("GET " + resource + " HTTP/1.1" + CRLF
-					+ "Host: " + hostName + CRLF + CRLF);
+            outputStream.writeBytes("GET " + resource + " HTTP/1.1" + CRLF
+                    + "Host: " + hostName + CRLF + CRLF);
 
-			String s = bufferedReader.readLine();
-			header.setLocation(hostName);
-			while (!s.equals("")) {
-				System.out.println(s);
-				s = bufferedReader.readLine();
-				if (s.contains(Constants.CONTENT_TYPE)) {
-					boolean added = header.setContentType(s.substring(
-							s.indexOf(":") + 1).trim());
-					if (!added) {
-						System.out
-								.println("Content-Type was not valid.\nExiting");
-					}
-				}
-				if (s.contains(Constants.CONTENT_LENGTH)) {
+            String s = bufferedReader.readLine();
+            header.setLocation(hostName);
+            while (!s.equals("")) {
+                System.out.println(s);
+                s = bufferedReader.readLine();
+                if (s.contains(Constants.CONTENT_TYPE)) {
+                    boolean added = header.setContentType(s.substring(
+                            s.indexOf(":") + 1).trim());
+                    if (!added) {
+                        System.out
+                                .println("Content-Type was not valid.\nExiting");
+                    }
+                }
+                if (s.contains(Constants.CONTENT_LENGTH)) {
 
-					header.setContentLength(Integer.parseInt(s.substring(
-							s.indexOf(":") + 1).trim()));
-				}
-				if (s.contains(Constants.TRANSFER_ENCODING)) {
-					if (s.contains("chunked")) {
-						header.setChunkedEncoding(true);
-					}
-				}
-			}
-			System.out.println("done");
+                    header.setContentLength(Integer.parseInt(s.substring(
+                            s.indexOf(":") + 1).trim()));
+                }
+                if (s.contains(Constants.TRANSFER_ENCODING)) {
+                    if (s.contains("chunked")) {
+                        header.setChunkedEncoding(true);
+                    }
+                }
+            }
+            System.out.println("done");
 
-			if (header.contentType == MimeType.png) {
-				savePNG(bufferedReader, header);
-			}
+            if (header.contentType == MimeType.png) {
+                savePNG(bufferedReader, header);
+            }
 
-			// TODO call you method here
+            // TODO call you method here
+            parseHtml();
 
-		} catch (SocketTimeoutException ste) {
-			System.out.println("connection timed out");
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				fos.close();
-				// inputStream.close();
-				bufferedReader.close();
-				outputStream.close();
-				socket.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+        } catch (SocketTimeoutException ste) {
+            System.out.println("connection timed out");
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fos.close();
+                // inputStream.close();
+                bufferedReader.close();
+                outputStream.close();
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
-		}
-	}
+        }
+    }
 
-	private static void savePNG(BufferedReader br, HTTPHeader header) {
-		FileOutputStream pngStream = null;
-		DataInputStream inputStream = null;
-		try {
-			inputStream = new DataInputStream(socket.getInputStream());
-			File pngFile = new File("abc.png");
-			pngStream = new FileOutputStream(pngFile);
+    private static void savePNG(BufferedReader br, HTTPHeader header) {
+        FileOutputStream pngStream = null;
+        DataInputStream inputStream = null;
+        try {
+            inputStream = new DataInputStream(socket.getInputStream());
+            File pngFile = new File("abc.png");
+            pngStream = new FileOutputStream(pngFile);
 
-			// int bytesRead = 0;
-			// while(bytesRead < header.getContentLength()){
-			// byte content = (byte) br.read();
-			// pngStream.write(content);
-			// bytesRead++;
-			// }
+            // int bytesRead = 0;
+            // while(bytesRead < header.getContentLength()){
+            // byte content = (byte) br.read();
+            // pngStream.write(content);
+            // bytesRead++;
+            // }
 
+            int bytesRead = 0;
+            while (bytesRead < header.getContentLength()) {
+                pngStream.write(inputStream.read());
+                bytesRead++;
+            }
 
-			int bytesRead = 0;
-			while (bytesRead < header.getContentLength()) {
-				pngStream.write(inputStream.read());
-				bytesRead++;
-			}
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } finally {
+            try {
+                if (pngStream != null) {
+                    pngStream.close();
+                }
 
+                if (inputStream != null) {
+                    inputStream.close();
+                }
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
 
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} finally {
-			try {
-				if (pngStream != null) {
-					pngStream.close();
-				}
+    }
 
-				if (inputStream != null) {
-					inputStream.close();
-				}
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
+    private static void parseHtml() {
+        if (header.chunkedEncoding) {
+            parseChunkedHtml();
+        } else {
+            parseUnchunckedHtml();
+        }
 
-	}
+    }
+
+    private static void parseChunkedHtml() {
+
+        PrintWriter mWriter = null;
+
+        try {
+            mWriter = new PrintWriter(textFile);
+            for (String line; (line = bufferedReader.readLine()) != null;) {
+
+                try {
+                    if (Integer.parseInt(line) == 0) {
+                        break;
+                    }
+
+                } catch (NumberFormatException nfe) {
+                    // Ignore this exception
+                }
+
+                mWriter.append(line);
+            }
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } finally {
+            if (mWriter != null) {
+                mWriter.close();
+            }
+        }
+
+    }
+
+    private static void parseUnchunckedHtml() {
+
+        PrintWriter mWriter = null;
+
+        int expectedCount = header.contentLength;
+
+        try {
+            mWriter = new PrintWriter(textFile);
+            for (int i = 0; i < expectedCount; i++) {
+                char toPrint = (char) bufferedReader.read();
+                mWriter.append(toPrint);
+            }
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } finally {
+            if (mWriter != null) {
+                mWriter.close();
+            }
+        }
+    }
 }
